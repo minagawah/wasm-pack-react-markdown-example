5db4a75e54ce8ad184ff860e1992a04695cdc0b0
minagawah <minagawa@tokyo800.jp>
2020-09-04 08:35:09 +0900

    doc: Modified information

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom';
 import logo from './logo.svg';
 
 import { Home } from 'components/home';
@@ -8,18 +12,20 @@ import { Experiments } from 'components/experiments';
 import 'styles.css';
 
 export const App = () => (
-  <div className="app">
-    <header>
-      <Link to='/'>
-        <img src={logo} className="logo" alt="logo" />
-      </Link>
-      <Link to='/'>Top</Link>
-      <Link to='/experiments'>Experiments</Link>
-    </header>
+  <Router>
+    <div className="app">
+      <header>
+        <Link to='/'>
+          <img src={logo} className="logo" alt="logo" />
+        </Link>
+        <Link to='/'>Top</Link>
+        <Link to='/experiments'>Experiments</Link>
+      </header>
 
-    <div className="container">
-      <Route exact path='/' component={Home} />
-      <Route path='/experiments' component={Experiments} />
+      <div className="container">
+        <Route exact path='/' component={Home} />
+        <Route path='/experiments' component={Experiments} />
+      </div>
     </div>
-  </div>
+  </Router>
 );
--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 
 import { App } from 'app';
 import * as serviceWorker from 'serviceWorker';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
